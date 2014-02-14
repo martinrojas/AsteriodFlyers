13740768753d9e2d4b5cf00151492f2c640cf28b
martinrojas <martin.rojas@live.com>
2014-02-14 12:00:02 -0500

    added the node js game logic initializer

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var path = require('path');
 // var routes = require('./routes');
 // var user = require('./routes/user');
 
+// Import the Anagrammatix game file.
+var agx = require('./agxgame');
+
+
 var app = express();
 
 // all environments
@@ -22,3 +26,15 @@ if ('development' == app.get('env')) {
 }
 
 var server = require('http').createServer(app).listen(8080);
+
+// Create a Socket.IO server and attach it to the http server
+var io = require('socket.io').listen(server);
+
+// Reduce the logging output of Socket.IO
+io.set('log level',1);
+
+// Listen for Socket.IO Connections. Once connected, start the game logic.
+io.sockets.on('connection', function (socket) {
+    //console.log('client connected');
+    agx.initGame(io, socket);
+});
