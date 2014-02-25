98b67e755f751012a2927c8426f8767e1d01e26a
Martin <martin.rojas@live.com>
2014-02-25 18:20:55 -0500

    added uikit
    Adding the UI Kit in order to make the demo a more complete experience.

--- public/js/lensflare.js
+++ public/js/lensflare.js
@@ -64,7 +64,7 @@ function lensInit() {
 	// lights
 
 	var ambient = new THREE.AmbientLight( 0xffffff );
-	ambient.color.setHSL( 0.1, 0.3, 0.2 );
+	ambient.color.setHSL( 0.1, 0.3, 1 );
 	scene.add( ambient );
 
 
